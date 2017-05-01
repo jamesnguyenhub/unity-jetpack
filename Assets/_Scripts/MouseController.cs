14538005586029c3c80bd1c11ee7673c3b78c8af
Tuyen Nguyen <nntuyen92@gmail.com>
2017-05-01 16:32:12 +0700

    Some decoration

---
+++ Assets/_Scripts/MouseController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+	public float jetpackForce = 75.0f;
+
+	private Rigidbody2D rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		bool jetpackActive = Input.GetButton("Fire1");
+
+		if (jetpackActive) {
+			rb.AddForce(new Vector2(0, jetpackForce));
+		}
+	}
+}
